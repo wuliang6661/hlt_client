e1f6ea09755fc132a59b91f8982aa8bff42b4669
wuliang6661 <wuliang6661@163.com>
2019-03-28 16:16:03 +0800

    修改代码

--- app/src/main/java/com/wul/hlt_client/ui/ordercommit/OrderCommitActivity.java
+++ app/src/main/java/com/wul/hlt_client/ui/ordercommit/OrderCommitActivity.java
@@ -534,8 +534,8 @@ public class OrderCommitActivity extends MVPBaseActivity<OrderCommitContract.Vie
         String now = sf.format(new Date());
         if (isNow(selectDate)) {   //如果时间是今天
             try {
-                Date startTime = new SimpleDateFormat(format).parse("06:00:00");
-                Date endTime = new SimpleDateFormat(format).parse("23:00:00");
+                Date startTime = new SimpleDateFormat(format).parse("11:00:00");
+                Date endTime = new SimpleDateFormat(format).parse("15:00:00");
                 Date nowTime = new SimpleDateFormat(format).parse(now);
                 if (isEffectiveDate(nowTime, startTime, endTime)) {
                     orderType = 1;
