925688c9a53b7fba2b90745081eb8d05d94d0fdc
wuliang <wy19941007>
2019-07-13 13:56:46 +0800

    休息休息

--- app/src/main/java/com/wul/hlt_client/api/HttpService.java
+++ app/src/main/java/com/wul/hlt_client/api/HttpService.java
@@ -56,7 +56,7 @@ import rx.Observable;
 
 public interface HttpService {
 
-    String URL = "http://47.98.53.141:8099";   //测试服
+    String URL = "http://47.103.7.183:8080";   //测试服
 //    String URL = "http://192.168.1.200:8091/";  //测试服2
 //    String URL = "http://api.open.yinghezhong.com/";  //正式环境
 //    String URL = "http://mapi.open.yinghezhong.com/";  //正式环境2
